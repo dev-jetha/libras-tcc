de28e47ad739f15154db8d44bcb1044dc2b6e554
dev-jetha <samueldavidsti@gmail.com>
2026-09-04 02:19:09 +0000

    feat: adicionar lógica de páginas e arrays no React

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,37 @@
+function nomeDaPagina(pagina){
+    if(pagina === "inicio"){
+        return "pagina inicial"
+    } 
+    if (pagina === "dicionario"){
+        return "dicionario de libras"
+    }
+    return "pagina em desenvolvimento"
+}
+
 function Home({pagina}){
+    const paginas = ["inicio", "libras", "dicionario", "jogos", "progresso", "configuracoes"]
+    console.log(paginas)
+    console.log(paginas[0])
+    console.log(paginas[1])
+
+    let conteudo
+
+    if(pagina === "inicio"){
+        conteudo = "Bem-vindo ao TCC Libras"
+    } else if(pagina === "dicionario"){
+        conteudo = "dicionario de libras"
+    } else {
+        conteudo = "pagina em desenvolvimento"
+    }
+        
+
+    const tituloPagina = nomeDaPagina(pagina)
+
+
     return(
         <main>
-            <h1>Bem-vindo ao TCC LIBRAS</h1>
+            <p>{tituloPagina}</p>
+            <h1>{conteudo}</h1>
             <p>Comece sua jornada de aprendizado.</p>
             <p>Página atual: {pagina}</p>
         </main>
